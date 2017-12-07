78f389b885b3b6b57f0e87ee451452524df19960
tooString <zgfazi@163.com>
2017-12-07 08:04:08 +0800

    add user manage and live page

--- other_code/edu/edu/app.py
+++ other_code/edu/edu/app.py
@@ -22,10 +22,11 @@ def register_extensions(app):
 
 
 def register_blueprints(app):
-    from .handlers import front, course, admin
+    from .handlers import front, course, admin, live
     app.register_blueprint(front)
     app.register_blueprint(course)
     app.register_blueprint(admin)
+    app.register_blueprint(live)
 
 
 def create_app(config):
--- other_code/edu/edu/handlers/admin.py
+++ other_code/edu/edu/handlers/admin.py
@@ -5,8 +5,8 @@ from flask import render_template
 from flask import request, current_app
 from flask import redirect, url_for, flash
 from edu.decorators import admin_required
-from edu.models import db, Course
-from edu.forms import CourseForm
+from edu.models import db, Course, User
+from edu.forms import CourseForm, RegisterForm
 
 admin = Blueprint('admin', __name__, url_prefix='/admin')
 
@@ -60,3 +60,55 @@ def delete_course(course_id):
     db.session.commit()
     flash('课程删除成功', 'success')
     return redirect(url_for('admin.courses'))
+
+
+@admin.route('/users')
+@admin_required
+def users():
+    page = request.args.get('page', default=1, type=int)
+    pagination = User.query.paginate(
+        page=page,
+        per_page=current_app.config['ADMIN_PER_PAGE'],
+        error_out=False
+    )
+    return render_template('admin/users.html', pagination=pagination)
+
+
+@admin.route('/users/create', methods=['GET', 'POST'])
+@admin_required
+def create_user():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        form.create_user()
+        flash('create user success', 'succcess')
+        return redirect(url_for('admin.users'))
+    return render_template('admin/create_user.html', form=form)
+
+
+@admin.route('/admin/<int:user_id>/edit', methods=['GET', 'POST'])
+@admin_required
+def edit_user(user_id):
+    user = User.query.get_or_404(user_id)
+    form = RegisterForm(obj=user)
+    if form.is_submitted():
+        form.populate_obj(user)
+        db.session.add(user)
+        try:
+            db.session.commit()
+        except FileExistsError:
+            db.session.rollback()
+            flash('username or email already exist', 'error')
+        else:
+            flash('user info update success', 'success')
+            return redirect(url_for('admin.users'))
+    return render_template('admin/edit_user.html', form=form, user=user)
+
+
+@admin.route('/users/<int:user_id>/delete', methods=['GET', 'POST'])
+@admin_required
+def delete_user(user_id):
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    flash('用户已经被删除', 'success')
+    return redirect(url_for('admin.users'))
